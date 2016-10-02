caca07c302e647d73a9c992ab2ebb1b576a15c3b
Cruel22 <molo4nig@gmail.com>
2016-10-02 21:16:42 +0300

    problem with ngstore

--- angular.js
+++ angular.js
@@ -13,7 +13,9 @@ ang.controller('myCtrl',   function(
 ){
 	$scope.myfact = myfact;
 	$scope.storage = $localStorage;
-	this.addEvent = myfact.addEvent(this.todo);
+	this.addEvent = function(){
+		myfact.addEvent(this.todo);
+	}
 	
 });
 
@@ -42,7 +44,8 @@ ang.directive('myInput',  function(){
 			element.bind("keydown keypress", function (event) {
             if(event.which === 13) {
                 scope.$apply(function (){
-                	myCtrl.addEvent();
+                	
+                	console.log(myCtrl);
                 });
 
                 event.preventDefault();
@@ -54,9 +57,12 @@ ang.directive('myInput',  function(){
 
 ang.factory('myfact',  function(){
 	var logic = {};
-	logic.addEvent = function ( ) {
-		console.log('Add Event function');
-	}
+
+	logic.addEvent = function (qwe) {
+		$storage.push(qwe);
+		console.log('Add Event function',qwe);
+	};
+
 	return logic;
 })
 
