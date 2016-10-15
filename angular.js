0135e8662a80fc65420042b6bef38b23490f4042
Cruel <molo4nig@gmail.com>
2016-10-15 14:07:44 +0300

    works

--- angular.js
+++ angular.js
@@ -1,106 +1,107 @@
-var ang= /**
-* myapp Module
-*
-* Description
-*/
-angular.module('myapp', ['ngStorage']);
+var ang =
+    /**
+     * myapp Module
+     *
+     * Description
+     */
+    angular.module('myapp', ['ngStorage']);
 
-ang.controller('myCtrl',   function(
+ang.controller('myCtrl', function (
     $scope,
     logic
-){
-	$scope.logic = logic;
-	var storage = $scope.storage;
-    
-	
-    this.get = function() {
+) {
+    $scope.logic = logic;
+    var storage = $scope.storage;
+
+
+    this.get = function () {
         return logic.getlist();
     }
-    
-    var	lists = this.get();
+
+    var lists = this.get();
     this.lists = lists;
-    
+
     this.add = function () {
         logic.addEvent(this.todo);
         this.todo = '';
         this.lists = this.get();
         event.preventDefault();
-        
-    };
-    this.deleteItem = function(id){
-        logic.deleteItem(id);
-    };
-    
-	
 
-    
-    
-    console.log('Controller info', Array.isArray(lists), typeof(lists),lists);
+    };
+    this.deleteItem = function (id) {
+        logic.deleteItem(id);
+        this.lists = this.get();
+    };
+
+
+
+    console.log('Controller info', Array.isArray(lists), typeof (lists), lists);
 });
 
-ang.factory('logic', ['$localStorage', function($localStorage){
-	var logic = {};
-    
+ang.factory('logic', ['$localStorage', function ($localStorage) {
+    var logic = {};
+
     //$localStorage.$reset();
     var stor = $localStorage;
     //console.log('store size',stor.storage.length);
-    
-	logic.addEvent =  function (qwe) {
-            if( typeof stor.storage == "undefined" ){
+    logic.addEvent = function (qwe) {
+        if (typeof stor.storage == "undefined") {
             var counter = 0;
-        }
-        else {
-            var counter = stor.storage.slice(-2, -1);
-        }
-           counter ++;
-            var input = {
-                name:qwe,
-                status: 'in-progress',
-                id:counter
+        } else {
+            let count =-1;
+            while((stor.storage.slice(count-1, count) != ":")&&(count >= -10)){
+                count --;
             }
-            if( typeof stor.storage == "undefined" ){
-                stor.storage = JSON.stringify(input);
-            }
-            else {
-                stor.storage = stor.storage + ',' + JSON.stringify(input);
-            }
-            //stor.storage = stor.storage + input;
-            //console.log(localStorage.storage);
-        
-	};
-	
-    logic.getlist = function (){
-        let result ='[' + stor.storage + ']'; 
-        
-        if(stor.storage === undefined){
-            return [];
+            var counter = stor.storage.slice(count, -1);
+            
+                }
+        counter++;
+        let input = {
+            name: qwe,
+            status: 'in-progress',
+            id: counter
         }
-        else {
-            return JSON.parse(result); 
+        if (typeof stor.storage == "undefined") {
+            stor.storage = JSON.stringify(input);
+        } else {
+            stor.storage = stor.storage + ',' + JSON.stringify(input);
         }
+        //stor.storage = stor.storage + input;
+        //console.log(localStorage.storage);
+
     };
     
-    logic.deleteItem = function(id){
+    logic.getlist = function () {
+        let result = '[' + stor.storage + ']';
         
-        let data = JSON.parse( '[' + stor.storage + ']') ;
-        for(let inc =0; inc <= data.length-1; inc++){
-            if(inc == 0){
-                    if(data[inc].id != id){
-                        stor.storage = JSON.stringify(data[inc]);
-                    }
-                
-            }
-            else{
-                if((data[inc].id !=id)){
-                    stor.storage = stor.storage + ',' + JSON.stringify(data[inc]);
-                }
-               
-            }
+        if (stor.storage === undefined) {
+            return [];
+        } else {
+            return JSON.parse(result);
         }
-        console.log(stor.storage);
-       
-       
+    };
+
+    logic.deleteItem = function (id) {
+
+        let data = JSON.parse('[' + stor.storage + ']');
+        $localStorage.$reset();
+        for (let inc = 0; inc <= data.length - 1; inc++) {
+            if (typeof (stor.storage) == "undefined") {
+                if (data[inc].id != id) {
+                    stor.storage = JSON.stringify(data[inc]);
+                }
+            } else {
+                if (data[inc].id != id) {
+                    stor.storage = stor.storage + ',' + JSON.stringify(data[inc]);
+                    console.log(data[inc].id , id);
+                }
+            }
+
+        }
+        console.log('data:' , data , '\n storage:', stor.data);
+
+
     }
 
-	return logic;
+    return logic;
 }])
